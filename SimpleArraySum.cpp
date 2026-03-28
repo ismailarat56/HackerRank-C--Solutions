f250581e91df5b0a3dfbf4d3051b140e7fb71d75
İsmail Arat <isko6isko5@gmail.com>
2026-03-28 16:24:46 +0300

    Refactor comments and maintain original functionality

--- SimpleArraySum.cpp
+++ SimpleArraySum.cpp
@@ -12,7 +12,7 @@ string ltrim(const string &);
 string rtrim(const string &);
 vector<string> split(const string &);
 
-//
+//----- BENİM ÇÖZÜMÜM BURADAN BAŞLIYOR -----
 int simpleArraySum(vector<int> ar) {
 int sum=0;
 for(int i=0;i<ar.size();i++){
@@ -20,7 +20,7 @@ for(int i=0;i<ar.size();i++){
 }
 return sum;
 }
-
+//-------- BENİM ÇÖZÜMÜM BURADA BİTİYOR --------
 int main()
 {
     ofstream fout(getenv("OUTPUT_PATH"));
